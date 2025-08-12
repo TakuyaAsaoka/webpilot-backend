7abe7e13fb01a5cc208e95ecaa4b0d1c61300382
Asaoka Takuya <dimas080201@gmail.com>
2025-08-12 13:12:02 +0900

    initial commit

---
+++ src/main/kotlin/com/asaoka/webpilot_backend/WebpilotBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.asaoka.webpilot_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebpilotBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebpilotBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/asaoka/webpilot_backend/WebpilotBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.asaoka.webpilot_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebpilotBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
